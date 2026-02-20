24ffba4356aa0e928024dc504165a4b91df4c54e
ajay kale <ajaykale749810@gmail.com>
2026-02-20 14:24:24 +0530

    Upadate auth Repository code

--- repo/authRepository.js
+++ repo/authRepository.js
@@ -10,3 +10,15 @@ exports.insertUser = (name, password, callback) => {
     return callback(null, result);
   });
 };
+
+exports.findUserByName = (name, callback) => {
+  const sql = "SELECT * FROM auth WHERE name = ? LIMIT 1";
+  db.query(sql, [name], (err, results) => {
+    if (err) {
+      console.error("Lookup failed:", err.message);
+      return callback(err);
+    }
+    const user = results && results.length > 0 ? results[0] : null;
+    return callback(null, user);
+  });
+};
