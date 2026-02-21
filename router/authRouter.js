3630497064e17472601d6ee2cb2bc17a8a948fa8
ajay kale <ajaykale749810@gmail.com>
2026-02-21 21:05:26 +0530

    Update router file

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/authController");
+const { verifyToken } = require("../middleware/authMiddleware");
 
 router.post("/", authController.addUser);
+router.post("/login", authController.loginUser);
+
+// Protected route: requires Authorization: Bearer <token>
+router.get("/me", verifyToken, authController.getMe);
+
 module.exports = router;
