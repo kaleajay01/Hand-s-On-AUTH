fe3ce809d4aa160c7ad02f47fadb83a83217a7fa
ajay kale <ajaykale749810@gmail.com>
2026-03-06 17:35:20 +0530

    update code

--- services/authServices.js
+++ services/authServices.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 
 
 exports.addUser = (name, password, callback) => {
-  // Hash password before saving
   bcrypt
     .hash(password, 10)
     .then((hashedPassword) => {
